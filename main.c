7b424ae0d0cb7c27de8841e8ae89994be62e87e6
smeh09 <shauryameh09@gmail.com>
2022-05-13 18:13:05 +0530

    done but one bug

--- main.c
+++ main.c
@@ -4,11 +4,16 @@ static int board[3][3];
 
 void emptyBrd();
 void renderBrd();
+int checkWin();
 
 int main() {
   char playing = 'Y';
   int pos;
   char validPos = 'N';
+  int leftpos = 9;
+  char xWon;
+  char oWon;
+  int gamedone = 0;
 
   if (playing == 'Y') {
     emptyBrd();
@@ -16,7 +21,7 @@ int main() {
 
   while (playing == 'Y') {
     validPos = 'N';
-    while (validPos == 'N') {
+    while (validPos == 'N' && leftpos > 0) {
       renderBrd();
 
       printf("Please enter where you want to place X ");
@@ -27,13 +32,21 @@ int main() {
           if (pos == board[y][x]) {
             board[y][x] = 100;
             validPos = 'Y';
+            leftpos--;
           }
         }
       }
     }
 
+    xWon = checkWin(100);
+    if (xWon == 0) {
+      gamedone = 1;
+      printf("X WON!! ");
+      goto newgame;
+    }
+
     validPos = 'N';
-    while (validPos == 'N') {
+    while (validPos == 'N' && leftpos > 0) {
       renderBrd();
 
       printf("Please enter where you want to place O ");
@@ -44,11 +57,68 @@ int main() {
           if (pos == board[y][x]) {
             board[y][x] = 200;
             validPos = 'Y';
+            leftpos--;
           }
         }
       }
     }
+    oWon = checkWin(200);
+    if (oWon == 0) {
+      gamedone = 1;
+      printf("O WON!!");
+      goto newgame;
+    }
+
+    newgame:
+      if (gamedone == 1) {
+        printf("\nDo you want to play another game? (Y/N) ");
+        scanf("%d", &playing);
+
+        if (playing == 'Y') {
+          gamedone = 0;
+        } else {
+          break;
+        }
+      }
   }
+  return 0;
+}
+
+int checkWin(int value) {
+  int vc = 1;
+  int hc = 1;
+  int drc = 1;
+  int dlc = 1;
+
+  if ((board[0][0] == value && board[1][0] == value && board[2][0] == value) ||
+  (board[0][1] == value && board[1][1] == value && board[2][1] == value) ||
+  (board[0][2] == value && board[1][2] == value && board[2][2] == value))
+   {
+     vc = 0;
+   }
+
+  if ((board[0][0] == value && board[0][1] == value && board[0][2] == value) ||
+  (board[1][0] == value && board[1][1] == value && board[1][2] == value) ||
+  (board[2][0] == value && board[2][1] == value && board[2][2] == value))
+  {
+    hc = 0;
+  }
+
+  if (board[0][0] == value && board[1][1] == value && board[2][2] == value)
+  {
+    drc = 0;
+  }
+
+  if (board[0][2] == value && board[1][1] == value && board[2][0] == value)
+  {
+    dlc = 0;
+  }
+
+  if (dlc == 0 || drc == 0 || hc == 0 || vc == 0) {
+    return 0;
+  }
+
+  return 1;
 }
 
 void emptyBrd() {
@@ -63,7 +133,7 @@ void emptyBrd() {
 
 void renderBrd() {
   printf("\n");
-  
+
   for (int y = 0; y < 3; y++) {
     printf("| ");
     for (int x = 0; x < 3; x++) {
