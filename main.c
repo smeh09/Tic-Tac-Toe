8bca4c96129b900827ea8352933fc41d855ea135
smeh09 <shauryameh09@gmail.com>
2022-05-13 17:26:05 +0530

    choice

--- main.c
+++ main.c
@@ -6,8 +6,27 @@ void emptyBrd();
 void renderBrd();
 
 int main() {
-  emptyBrd();
-  renderBrd();
+  char playing = 'Y';
+  int pos;
+
+  if (playing == 'Y') {
+    emptyBrd();
+  }
+
+  while (playing == 'Y') {
+    renderBrd();
+
+    printf("Please enter where you want to place X ");
+    scanf("%d", &pos);
+
+    for (int y = 0; y < 3; y++) {
+      for (int x = 0; x < 3; x++) {
+        if (pos == board[y][x]) {
+          board[y][x] = 100;
+        }
+      }
+    }
+  }
 }
 
 void emptyBrd() {
@@ -24,7 +43,13 @@ void renderBrd() {
   for (int y = 0; y < 3; y++) {
     printf("| ");
     for (int x = 0; x < 3; x++) {
-      printf("%d | ", board[y][x]);
+      if (board[y][x] != 100 && board[y][x] != 200) {
+        printf("%d | ", board[y][x]);
+      } else if (board[y][x] == 100) {
+        printf("X | ");
+      } else if (board[y][x] == 200) {
+        printf("O | ");
+      }
     }
     if (y != 2) {
       printf("\n --- --- ---\n");
